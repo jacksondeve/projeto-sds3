9b1548596c09ea9a591de9aa883099c571373f7c
jacksondeve <jknmdo16@hotmail.com>
2021-05-12 18:52:32 -0300

    project clean3

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -6,9 +6,8 @@ const Footer = () => {
 
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/jacksondeve/projeto-sds3" target="_blank" rel="noreferrer">jackson mateus</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                Evento promovido pela escola DevSuperior: <a href="" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <h3>jackson mateus</h3>
+               
             </div>
         </footer>
 
